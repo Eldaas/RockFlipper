2af74806630bfeba5d9aaa176b863a0c227feeb6
Eldaas <danieljones91@gmail.com>
2021-03-31 17:20:04 +0800

    Update asteroid physics interaction (#8)
    - Implements AsteroidData scriptable object to serve as a data profile (to hold run-time material references and various common settings) for asteroid hazard objects.
    - Implements HazardManager class to act as a monobehaviour component to input & reference scriptable object data files for hazards.
    - Increases physics mass on all asteroids and shards.
    - Increases physics mass on player ship.
    - Changes camera perspective to top-down orthographic in PrototypeLevel scene.
    - Freezes asteroid rigidbody Y position.
    - Fixes spawner positioning and offset setting.

---
+++ Assets/Scripts/Scriptable Objects/AsteroidData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hazard Data/Asteroid Data")]
+public class AsteroidData : ScriptableObject
+{
+    [Header("Inspector References")]
+    public Material ironMaterial;
+    public Material silverMaterial;
+    public Material goldMaterial;
+    public float collisionSensitivity;
+}
