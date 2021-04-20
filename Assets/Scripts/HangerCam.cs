9fa21b526451f1e092c404e6a48c609180adc381
EroR43 <ero43000@gmail.com>
2021-04-20 16:30:26 +0800

    Hanger Set Up
    This is the first addition of the hanger to the project + camera controls for moving between different points in the hanger.

---
+++ Assets/Scripts/HangerCam.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangerCam : MonoBehaviour
+{
+    private GameObject cam;
+    public List<Transform> camSpots = new List<Transform>();
+    [SerializeField]
+    private int spotInd = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            spotInd += 1;
+            if (spotInd > 3)
+            {
+                spotInd = 0;
+            }
+            cam.transform.position = camSpots[spotInd].position;
+            cam.transform.rotation = camSpots[spotInd].rotation;
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            spotInd -= 1;
+            if (spotInd < 0)
+            {
+                spotInd = 3;
+            }
+            cam.transform.position = camSpots[spotInd].position;
+            cam.transform.rotation = camSpots[spotInd].rotation;
+        }
+    }
+
+
+}
