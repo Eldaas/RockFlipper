5ad4dd2f7d89da0772a7f62263a0be2682f76a19
EroR43 <ero43000@gmail.com>
2021-05-13 23:20:30 +0800

    UI Implementation Update 2
    Images added and scaling adjusted, code implemented for all bars except velocity.

--- Assets/HealthRing.cs
+++ Assets/HealthRing.cs
@@ -12,14 +12,27 @@ public class HealthRing : MonoBehaviour
     private Player playerScript;
 
     public Image shieldFill;
+    public Image velocityFill;
+    public Image batteryFill;
+    public Image hullFill;
+    public Image armorFill;
 
     [SerializeField]
     private UnityAction shieldEventDelegate;
 
+    private void SetupFill()
+    {
+        shieldFill.fillAmount = 1;
+        velocityFill.fillAmount = 0;
+        batteryFill.fillAmount = 1;
+        hullFill.fillAmount = 1;
+        armorFill.fillAmount = 1;
+    }
+
     private void RegisterListeners()
     {
-        shieldEventDelegate = ShieldUpdate;
-        EventManager.StartListening("ShieldsHit", shieldEventDelegate);
+        //shieldEventDelegate = ShieldUpdate;
+        //EventManager.StartListening("ShieldsHit", shieldEventDelegate);
     }
 
     void ShieldUpdate()
@@ -27,10 +40,20 @@ public class HealthRing : MonoBehaviour
         shieldFill.fillAmount = playerScript.stats.currentShields / playerScript.stats.currentMaxShields;
         Debug.Log("Current shield Calc = " + shieldFill.fillAmount);
     }
-    
+
+    void UpdateFills()
+    {
+        shieldFill.fillAmount = playerScript.stats.currentShields / playerScript.stats.currentMaxShields;
+        velocityFill.fillAmount = playerScript.Velocity / playerScript.stats.currentMaximumVelocity;
+        batteryFill.fillAmount = playerScript.stats.currentHeatSinkLevel / playerScript.stats.currentHeatSinkCapacity;
+        hullFill.fillAmount = playerScript.stats.currentHull / playerScript.stats.currentMaxHull;
+        armorFill.fillAmount = playerScript.stats.currentArmour / playerScript.stats.currentMaxArmour;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        SetupFill();
         player = GameObject.FindGameObjectWithTag("Player");
         playerScript = player.GetComponent<Player>();
         RegisterListeners();
@@ -43,6 +66,6 @@ public class HealthRing : MonoBehaviour
         tarPos.x = player.transform.position.x;
         tarPos.z = player.transform.position.z;
         transform.position = tarPos;
-        shieldFill.fillAmount = playerScript.stats.currentShields / playerScript.stats.currentMaxShields;
+        UpdateFills();
     }
 }
