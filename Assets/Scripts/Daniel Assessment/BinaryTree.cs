3f9ca52bce674a3b70bf8d73cd85c1d4c8aa8030
Eldaas <danieljones91@gmail.com>
2021-06-02 21:22:04 +0800

    WIP - Implements SortingSearchingDLL & BinaryTree class

---
+++ Assets/Scripts/Daniel Assessment/BinaryTree.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryTree
+{
+    // Tree constructors
+    public BinaryTree()
+    {
+
+    }
+
+    public BinaryTree(BinaryTreeNode _root)
+    {
+        root = _root;
+    }
+
+    public class BinaryTreeNode
+    {
+        public int index;
+        public Object data;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+    }
+
+    public BinaryTreeNode root;
+
+    public bool CreateNodeAtIndex(int index)
+    {
+        BinaryTreeNode before = null;
+        BinaryTreeNode after = root;
+
+        while (after != null)
+        {
+            before = after;
+            if (index < after.index)
+            {
+                after = after.left;
+            }
+            else if (index > after.index)
+            {
+                after = after.right;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        BinaryTreeNode newNode = new BinaryTreeNode();
+        newNode.index = index;
+
+        if (root == null)
+        {
+            root = newNode;
+        }
+        else
+        {
+            if (index < before.index)
+            {
+                before.left = newNode;
+            }
+            else
+            {
+                before.right = newNode;
+            }
+        }
+
+        return true;
+    }
+
+    public bool CreateNode(BinaryTreeNode node)
+    {
+        if (node != null)
+        {
+            if (Find(node.index) == null)
+            {
+                BinaryTreeNode before = null;
+                BinaryTreeNode after = root;
+
+                while (after != null)
+                {
+                    before = after;
+
+                    if (node.index < after.index)
+                    {
+                        after = after.left;
+                    }
+                    else if (node.index > after.index)
+                    {
+                        after = after.right;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (root == null)
+                {
+                    root = node;
+                }
+                else
+                {
+                    if (node.index < before.index)
+                    {
+                        before.left = node;
+                    }
+                    else
+                    {
+                        before.right = node;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public BinaryTreeNode Find(int index)
+    {
+        return Find(index, root);
+    }
+
+    private BinaryTreeNode Find(int index, BinaryTreeNode parent)
+    {
+        if (parent != null)
+        {
+            if (index == parent.index)
+            {
+                return parent;
+            }
+            else if (index < parent.index)
+            {
+                return Find(index, parent.left);
+            }
+            else
+            {
+                return Find(index, parent.right);
+            }
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+    public void TraversePreOrder(BinaryTreeNode parent)
+    {
+        if (parent != null)
+        {
+            Debug.Log(parent.index);
+            TraversePreOrder(parent.left);
+            TraversePreOrder(parent.right);
+        }
+    }
+
+    public void TraversePostOrder(BinaryTreeNode parent)
+    {
+        if (parent != null)
+        {
+            TraversePostOrder(parent.left);
+            TraversePostOrder(parent.right);
+            Debug.Log(parent.index);
+        }
+    }
+
+    public void TraverseInOrder(BinaryTreeNode parent)
+    {
+        if (parent != null)
+        {
+            TraverseInOrder(parent.left);
+            Debug.Log(parent);
+            TraverseInOrder(parent.right);
+        }
+    }
+
+}
--- Assets/Scripts/Daniel Assessment/HighScores.cs
+++ Assets/Scripts/Daniel Assessment/HighScores.cs
@@ -3,12 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Runtime.InteropServices;
 
 [Serializable]
 public class HighScores : MonoBehaviour
 {
     [SerializeField]
     public DreamloData data;
+
+    private LinkedList<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> ascendingScores;
+    private LinkedList<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> ascendingTimes;
+    private BinaryTree dataTree;
+
+    #region External Methods
+    [DllImport("SortingSearchingDLL")]
+    private static extern void BubbleSort(int[] arr, int n);
+    [DllImport("SortingSearchingDLL")]
+    private static extern void QuickSort(int[] arr, int low, int high);
+
+    #endregion
 
     private void Awake()
     {
@@ -20,6 +33,7 @@ public class HighScores : MonoBehaviour
     {
         AddDummyScores();
         StartCoroutine(GetRequest("http://dreamlo.com/lb/60b5ed888f40bb64eca5f56d/json"));
+        
     }
 
     IEnumerator GetRequest(string url)
@@ -35,9 +49,12 @@ public class HighScores : MonoBehaviour
             }
             else
             {
+                // Seems to send an OK string before it sends the body text. STUPID. WASTED 4 HOURS OF MY TIME.
                 if(webRequest.downloadHandler.text != "OK")
                 {
                     data = JsonUtility.FromJson<DreamloData>(webRequest.downloadHandler.text);
+                    SortAscendingScore();
+                    SortAscendingTime();
                 }
             }
         }
@@ -55,7 +72,119 @@ public class HighScores : MonoBehaviour
         StartCoroutine(GetRequest($"http://dreamlo.com/lb/REyGVjWkkkqwY5J0ITSyWA4CP2veOrbU-1yqU3OD6r4A/add/{name}/{balance}/{totalTime}"));
     }
 
+    private void SortAscendingScore()
+    {
+        int[] scores = new int[data.dreamlo.leaderboard.entry.Count];
 
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = (int)data.dreamlo.leaderboard.entry[i].score;
+        }
+
+        BubbleSort(scores, scores.Length);
+
+        string scoresOutput = "Sorted scores: ";
+        foreach (int score in scores)
+        {
+            scoresOutput += ", " + score.ToString();
+        }
+        Debug.Log(scoresOutput);
+        ascendingScores = PairWithData(scores, true);
+
+        string sortedScoreRecords = "Sorted score records: ";
+        foreach(DreamloData.Dreamlo.Leaderboard.HighScoreRecord record in ascendingScores)
+        {
+            sortedScoreRecords += $"[Name: {record.name}, Score: {record.score}] ";
+        }
+        Debug.Log(sortedScoreRecords);
+
+        StoreDataInBinaryTree(ascendingScores, dataTree);
+        dataTree.TraverseInOrder(dataTree.root);
+    }
+
+    private void SortAscendingTime()
+    {
+        int[] times = new int[data.dreamlo.leaderboard.entry.Count];
+
+        for (int i = 0; i < times.Length; i++)
+        {
+            times[i] = (int)data.dreamlo.leaderboard.entry[i].seconds;
+        }
+
+        QuickSort(times, 0, times.Length);
+
+        string timesOutput = "Sorted times: ";
+        foreach(int time in times)
+        {
+            timesOutput += ", " + time.ToString();
+        }
+        Debug.Log(timesOutput);
+        ascendingTimes = PairWithData(times, false);
+
+        string sortedScoreRecords = "Sorted time records: ";
+        foreach (DreamloData.Dreamlo.Leaderboard.HighScoreRecord record in ascendingScores)
+        {
+            sortedScoreRecords += $"[Name: {record.name}, Score: {record.seconds}] ";
+        }
+        Debug.Log(sortedScoreRecords);
+    }
+
+    /// <summary>
+    /// Sorts the high score records into a doubly-linked list by matching them back up with their score, as sorted by the C++ DLL functions. Dirty but gets the job done.
+    /// </summary>
+    /// <param name="input">The sorted array of either scores or times</param>
+    /// <param name="isScore">If this is the score array, this should be true. If this is the times array, this should be false.</param>
+    /// <returns></returns>
+    LinkedList<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> PairWithData(int[] input, bool isScore)
+    {
+        List<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> workingList = new List<DreamloData.Dreamlo.Leaderboard.HighScoreRecord>(data.dreamlo.leaderboard.entry);
+        DreamloData.Dreamlo.Leaderboard.HighScoreRecord[] sortedRecords = new DreamloData.Dreamlo.Leaderboard.HighScoreRecord[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int w = 0; w < workingList.Count; w++)
+            {
+                if (isScore)
+                {
+                    if ((int)workingList[w].score == input[i])
+                    {
+                        sortedRecords[i] = workingList[w];
+                        workingList.RemoveAt(w);
+                        break;
+                    }
+                }
+                else
+                {
+                    if ((int)workingList[w].seconds == input[i])
+                    {
+                        sortedRecords[i] = workingList[w];
+                        workingList.RemoveAt(w);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new LinkedList<DreamloData.Dreamlo.Leaderboard.HighScoreRecord>(sortedRecords);
+    }
+
+    public void StoreDataInBinaryTree(LinkedList<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> data, BinaryTree tree)
+    {
+        LinkedListNode<DreamloData.Dreamlo.Leaderboard.HighScoreRecord> currentNode = data.Last;
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            BinaryTree.BinaryTreeNode node = new BinaryTree.BinaryTreeNode();
+            node.index = i;
+            node.data = (UnityEngine.Object)currentNode.Value;
+            tree.CreateNode(node);
+            currentNode = currentNode.Previous;
+        }
+    }
+
+    /// <summary>
+    /// Nested classes implementation
+    /// </summary>
     [Serializable]
     public class DreamloData
     {
@@ -69,10 +198,10 @@ public class HighScores : MonoBehaviour
             [Serializable]
             public class Leaderboard
             {
-                public List<Entry> entry;
+                public List<HighScoreRecord> entry;
 
                 [Serializable]
-                public class Entry
+                public class HighScoreRecord
                 {
                     public string name;
                     public float score;
@@ -83,27 +212,4 @@ public class HighScores : MonoBehaviour
             }
         }
     }
-
-    
-
-    
-
-    
-
-
-
-
-
-    // Upload Dreamlo data
-
-    // Load Dreamlo data
-
-    // Split data into list
-
-    // Sort data ascending score into first doubly-linked list
-
-    // Sort data ascending play time into second doubly-linked list
-
-    // Split list into two doubly-linked lists (score, total play time)
-
 }
